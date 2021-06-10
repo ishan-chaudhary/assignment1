d963b03f2c200b8e90815f619671931be4075fc7
ishan <ishan4220@gmail.com>
2021-06-10 18:24:29 +0530

    Dynamic truncating threshold

--- script.js
+++ script.js
@@ -1,37 +1,75 @@
 var currentActiveNode = null;
 var currentImageIndex = 0;
 
-function truncateString(str) {
-  if (str.length > 35) {
-    return str.substr(0, 15).trim() + "....." + str.substr(str.length - 13, str.length);
+var makeInt = function (value) {
+  if(typeof(value)==='string'){
+      value = Number(value.replace(/[^0-9\.]+/g,""));
   }
-  return str;
+  var int = parseInt(value, 10);
+  return int;
+};
+
+function getLetterSpacing(previewTextHolder){
+    let temp = window.getComputedStyle(previewTextHolder, null).getPropertyValue('letter-spacing');
+    if(temp=='normal'){
+        temp = 0;
+    }else{
+        temp = makeInt(temp);
+    }
+    return temp;
 }
 
-window.onload = function () { // Waiting for DOM to load
+function truncate(previewRow, previewTextHolder, previewText) {
+  var numChars = 0;
+  let elFontSize = window
+    .getComputedStyle(previewTextHolder, null)
+    .getPropertyValue("font-size");
+  let temp = getLetterSpacing(previewTextHolder);
+  var letterSpace = makeInt(elFontSize) + temp;
+  var boxCenter = makeInt(previewRow.scrollWidth) / 2;
+  for (var l = letterSpace * 2; l < boxCenter; l += letterSpace) {
+    numChars = numChars + 1;
+  }
+
+  if (
+    letterSpace * previewText.length > previewRow.scrollWidth &&
+    2 * numChars + 4 < previewText.length
+  ) {
+    return (
+      previewText.substr(0, numChars).trim() +
+      "...." +
+      previewText.substr(previewText.length - numChars, previewText.length)
+    );
+  }
+  return previewText;
+}
+
+window.onload = function () {
+  // Waiting for DOM to load
   var previewContainer =
     document.getElementsByClassName("preview-container")[0]; // Select Preview Container
 
   var imageOutput = document.getElementsByClassName("image-out")[0];
-  var imageTitle = document.getElementById('image-out-title');
+  var imageTitle = document.getElementById("image-out-title");
 
   for (let image in images) {
-    let previewRow = document.createElement("div");  // Used to create an element
+    let previewRow = document.createElement("div"); // Used to create an element
 
     previewRow.setAttribute("class", "preview-row"); // Set Class property
     previewRow.setAttribute("data-image", images[image].previewImage);
     previewRow.setAttribute("data-title", images[image].title);
 
-    previewRow.addEventListener("click", () => {     // On click logic 
-        if(currentActiveNode){
-            currentActiveNode.classList.remove('active'); //Add and remove active class to show currently active tab
-        }
-        imageOutput.style.backgroundImage =
-        "url(" + images[image].previewImage + ")";   // set background image of the image-output div
-        imageTitle.innerText = images[image].title
-        currentActiveNode = previewRow;
-        currentActiveNode.classList.add('active');
-        currentImageIndex = Number(image);
+    previewRow.addEventListener("click", () => {
+      // On click logic
+      if (currentActiveNode) {
+        currentActiveNode.classList.remove("active"); //Add and remove active class to show currently active tab
+      }
+      imageOutput.style.backgroundImage =
+        "url(" + images[image].previewImage + ")"; // set background image of the image-output div
+      imageTitle.innerText = images[image].title;
+      currentActiveNode = previewRow;
+      currentActiveNode.classList.add("active");
+      currentImageIndex = Number(image);
     });
 
     let previewImage = document.createElement("span");
@@ -41,11 +79,13 @@ window.onload = function () { // Waiting for DOM to load
 
     let previewText = document.createElement("span");
     previewText.setAttribute("class", "preview-text");
-    previewText.innerText = truncateString(images[image].title);
+    previewText.innerText = images[image].title
 
-    previewRow.appendChild(previewImage);  // appending out newly created node elements to the DOM parent
+    previewRow.appendChild(previewImage); // appending out newly created node elements to the DOM parent
     previewRow.appendChild(previewText);
     previewContainer.appendChild(previewRow);
+
+    previewText.innerText = truncate(previewRow,previewText,images[image].title);
   }
 
   catchKeyStroke();
@@ -53,37 +93,46 @@ window.onload = function () { // Waiting for DOM to load
 
 // Function to facilitate key pressing logic
 
-function catchKeyStroke(){
-    var imageOutput = document.getElementsByClassName("image-out")[0];
-    let previewRows = document.getElementsByClassName('preview-row');
-    var imageTitle = document.getElementById('image-out-title');
+function catchKeyStroke() {
+  var imageOutput = document.getElementsByClassName("image-out")[0];
+  let previewRows = document.getElementsByClassName("preview-row");
+  var imageTitle = document.getElementById("image-out-title");
 
-    currentActiveNode = previewRows[currentImageIndex];
-    imageOutput.style.backgroundImage = "url(" + currentActiveNode.getAttribute('data-image')+ ")";
-    imageTitle.innerText = currentActiveNode.getAttribute('data-title')
-    currentActiveNode.classList.add('active');
+  currentActiveNode = previewRows[currentImageIndex];
+  imageOutput.style.backgroundImage =
+    "url(" + currentActiveNode.getAttribute("data-image") + ")";
+  imageTitle.innerText = currentActiveNode.getAttribute("data-title");
+  currentActiveNode.classList.add("active");
 
-    document.addEventListener('keydown',(event)=>{
-        event.preventDefault(); // Preventing any default behaviour of the key
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault(); // Preventing any default behaviour of the key
 
-        if(event.key === "ArrowUp"){        // To handle down key stroke
-            currentActiveNode.classList.remove('active');
-            currentImageIndex = currentImageIndex === 0 ? images.length - 1 : Number(currentImageIndex) - 1;
-            currentActiveNode = previewRows[currentImageIndex];
-            imageOutput.style.backgroundImage = "url(" + currentActiveNode.getAttribute('data-image')+ ")";
-            imageTitle.innerText = currentActiveNode.getAttribute('data-title');
-            currentActiveNode.classList.add('active');
-        }
-        else if(event.key === "ArrowDown"){  // To handle up key stroke
-            currentActiveNode.classList.remove('active');
-            currentImageIndex = currentImageIndex === images.length - 1 ? 0 : Number(currentImageIndex) + 1;
-            currentActiveNode = previewRows[currentImageIndex];
-            imageOutput.style.backgroundImage = "url(" + currentActiveNode.getAttribute('data-image')+ ")";
-            imageTitle.innerText = currentActiveNode.getAttribute('data-title');
-            currentActiveNode.classList.add('active');
-        }
-        else{
-            alert('Only Up and Down keys are allowed as input');  // Alerting user to use only up and down keys
-        }
-    })
+    if (event.key === "ArrowUp") {
+      // To handle down key stroke
+      currentActiveNode.classList.remove("active");
+      currentImageIndex =
+        currentImageIndex === 0
+          ? images.length - 1
+          : Number(currentImageIndex) - 1;
+      currentActiveNode = previewRows[currentImageIndex];
+      imageOutput.style.backgroundImage =
+        "url(" + currentActiveNode.getAttribute("data-image") + ")";
+      imageTitle.innerText = currentActiveNode.getAttribute("data-title");
+      currentActiveNode.classList.add("active");
+    } else if (event.key === "ArrowDown") {
+      // To handle up key stroke
+      currentActiveNode.classList.remove("active");
+      currentImageIndex =
+        currentImageIndex === images.length - 1
+          ? 0
+          : Number(currentImageIndex) + 1;
+      currentActiveNode = previewRows[currentImageIndex];
+      imageOutput.style.backgroundImage =
+        "url(" + currentActiveNode.getAttribute("data-image") + ")";
+      imageTitle.innerText = currentActiveNode.getAttribute("data-title");
+      currentActiveNode.classList.add("active");
+    } else {
+      alert("Only Up and Down keys are allowed as input"); // Alerting user to use only up and down keys
+    }
+  });
 }
