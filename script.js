5e0befd7bb300b0655afb4f6e0d6b8d3b89a7145
ishan <ishan4220@gmail.com>
2021-06-16 12:53:08 +0530

    truncating_using_css

--- script.js
+++ script.js
@@ -9,53 +9,55 @@ var makeInt = function (value) {
   return int;
 };
 
-function getLetterSpacing(previewTextHolder){
-    // As we have used external .css file thus we need to use window.getComputedStyle and not simple element.style to get value of a property.
-    let temp = window.getComputedStyle(previewTextHolder, null).getPropertyValue('letter-spacing'); // This fetches the letter-spacing property.
-    if(temp=='normal'){
-        temp = 0;
-    }else{
-        temp = makeInt(temp);
-    }
-    return temp;
-}
+/* Implemented the truncating functionality using css */
 
-function truncate(previewTextHolder, previewText) {
-  var numChars = 0;
-  let elFontSize = window
-    .getComputedStyle(previewTextHolder, null)
-    .getPropertyValue("font-size"); // Getting font-size property of the span containing the text
+// function getLetterSpacing(previewTextHolder){
+//     // As we have used external .css file thus we need to use window.getComputedStyle and not simple element.style to get value of a property.
+//     let temp = window.getComputedStyle(previewTextHolder, null).getPropertyValue('letter-spacing'); // This fetches the letter-spacing property.
+//     if(temp=='normal'){
+//         temp = 0;
+//     }else{
+//         temp = makeInt(temp);
+//     }
+//     return temp;
+// }
 
-  let temp = getLetterSpacing(previewTextHolder);
-  var letterSpace = makeInt(elFontSize) + temp;
+// function truncate(previewTextHolder, previewText) {
+//   var numChars = 0;
+//   let elFontSize = window
+//     .getComputedStyle(previewTextHolder, null)
+//     .getPropertyValue("font-size"); // Getting font-size property of the span containing the text
 
-  var boxCenter = makeInt(previewTextHolder.scrollWidth) / 2; // Get the width of the span element
-  /*
-    Logic for deciding number of characters
-        We first calculated how much space a character takes.
-        Our put string will be of the form 
-            x + y + z = width of span;
-            where x and z are the lenths of the character at the end and begining and y is the ellipse character.
-        Therefore in order to calculate the number of characters at the end and the starting of the string we would run the followinf for loop.
-        Now we check if the space taken by letters is greater than the width of the parent span element.
-        If yes then we truncate the the string into our format.
-  */ 
-  for (var l = letterSpace * 2; l < boxCenter; l += letterSpace) {
-    numChars = numChars + 1;
-  }
+//   let temp = getLetterSpacing(previewTextHolder);
+//   var letterSpace = makeInt(elFontSize) + temp;
 
-  if (
-    letterSpace * previewText.length > previewTextHolder.scrollWidth &&
-    2 * numChars + 4 < previewText.length
-  ) {
-    return (
-      previewText.substr(0, numChars).trim() +
-      "...." +
-      previewText.substr(previewText.length - numChars, previewText.length)
-    );
-  }
-  return previewText;
-}
+//   var boxCenter = makeInt(previewTextHolder.scrollWidth) / 2; // Get the width of the span element
+//   /*
+//     Logic for deciding number of characters
+//         We first calculated how much space a character takes.
+//         Our put string will be of the form 
+//             x + y + z = width of span;
+//             where x and z are the lenths of the character at the end and begining and y is the ellipse character.
+//         Therefore in order to calculate the number of characters at the end and the starting of the string we would run the followinf for loop.
+//         Now we check if the space taken by letters is greater than the width of the parent span element.
+//         If yes then we truncate the the string into our format.
+//   */ 
+//   for (var l = letterSpace * 2; l < boxCenter; l += letterSpace) {
+//     numChars = numChars + 1;
+//   }
+
+//   if (
+//     letterSpace * previewText.length > previewTextHolder.scrollWidth &&
+//     2 * numChars + 4 < previewText.length
+//   ) {
+//     return (
+//       previewText.substr(0, numChars).trim() +
+//       "...." +
+//       previewText.substr(previewText.length - numChars, previewText.length)
+//     );
+//   }
+//   return previewText;
+// }
 
 window.onload = function () {
   // Waiting for DOM to load
@@ -90,15 +92,24 @@ window.onload = function () {
     previewImage.style.backgroundImage =
       "url(" + images[image].previewImage + ")";
 
-    let previewText = document.createElement("span");
+    let previewText = document.createElement("div");
     previewText.setAttribute("class", "preview-text");
-    previewText.innerText = images[image].title
+    
+    let spanEl1 = document.createElement('span');
+    let spanEl2 = document.createElement('span');
 
+    spanEl1.innerText = images[image].title.substr(0,images[image].title.length/2);
+    spanEl2.innerText = images[image].title.substr(images[image].title.length/2,images[image].title.length);
+
+
+    // previewText.innerText = images[image].title
+    previewText.appendChild(spanEl1);
+    previewText.appendChild(spanEl2)
     previewRow.appendChild(previewImage); // appending out newly created node elements to the DOM parent
     previewRow.appendChild(previewText);
     previewContainer.appendChild(previewRow);
 
-    previewText.innerText = truncate(previewText,images[image].title);
+    // previewText.innerText = truncate(previewText,images[image].title);
   }
 
   catchKeyStroke();
